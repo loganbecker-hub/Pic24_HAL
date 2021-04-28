e3e4c4f2ac05bd6eafa81835700f0fa9d94da754
Logan Becker <logan.becker.77@gmail.com>
2021-04-28 13:42:11 +0200

    Clean up for GPIO_HAL Header file Description

--- GPIO_HAL.c
+++ GPIO_HAL.c
@@ -1,13 +1,6 @@
 #include "GPIO_HAL.h"
 #include <p24FJ64GA104.h> // Header file for specific PIC24
 
-/**
-  * @brief  Initializes the GPIOx peripheral
-  * @param  GPIOx: specify which Port to setup
-  * @param  GPIO_PIN: specify which Pin to setup
-  * @param  GPIO_MODE: specify pin as an Input or Output
-  * @retval None
-**/
 void HAL_GPIO_Init(uint16_t GPIOx, uint16_t GPIO_PIN, uint16_t GPIO_MODE){
   /** OUTPUT MODE **/
   if(GPIO_MODE == 0U){  
@@ -38,12 +31,6 @@ void HAL_GPIO_Init(uint16_t GPIOx, uint16_t GPIO_PIN, uint16_t GPIO_MODE){
   }
 }
 
-/**
-  * @brief  Toggle Pin for the GPIOx peripheral
-  * @param  GPIOx: Port to access
-  * @param  GPIO_PIN: Pin to toggle  
-  * @retval None
-**/
 void HAL_GPIO_TogglePin(uint16_t GPIOx, uint16_t GPIO_PIN){
   if(GPIOx == 0U){
     LATA ^= 1U<<GPIO_PIN; 
@@ -56,13 +43,6 @@ void HAL_GPIO_TogglePin(uint16_t GPIOx, uint16_t GPIO_PIN){
   }
 }
 
-/**
-  * @brief  Writes to Pin for the GPIOx peripheral
-  * @param  GPIOx: Port to access
-  * @param  GPIO_PIN: Pin to write 
-  * @param  GPIO_STATE: Set or Clear the pin 
-  * @retval None
-**/
 void HAL_GPIO_WritePin(uint16_t GPIOx, uint16_t GPIO_PIN, uint16_t GPIO_STATE){
   /** Set the Pin **/
   if(GPIO_STATE == 1U){ 
@@ -87,6 +67,5 @@ void HAL_GPIO_WritePin(uint16_t GPIOx, uint16_t GPIO_PIN, uint16_t GPIO_STATE){
     else if(GPIOx == 2U){
       LATC &= ~(1U<<GPIO_PIN);
     }
-  }
-   
+  } 
 }
--- GPIO_HAL.h
+++ GPIO_HAL.h
@@ -46,8 +46,31 @@ extern "C" {
 #define GPIO_INPUT  1U // all pins input
   
 /** GPIO Function Declarations **/
+
+/**
+  * @brief  Initializes the GPIOx peripheral
+  * @param  GPIOx: specify which Port to setup
+  * @param  GPIO_PIN: specify which Pin to setup
+  * @param  GPIO_MODE: specify pin as an Input or Output
+  * @retval None
+**/  
 void HAL_GPIO_Init(uint16_t GPIOx, uint16_t GPIO_PIN, uint16_t GPIO_MODE);
+
+/**
+  * @brief  Toggle Pin for the GPIOx peripheral
+  * @param  GPIOx: Port to access
+  * @param  GPIO_PIN: Pin to toggle  
+  * @retval None
+**/
 void HAL_GPIO_TogglePin(uint16_t GPIOx, uint16_t GPIO_PIN);
+
+/**
+  * @brief  Writes to Pin for the GPIOx peripheral
+  * @param  GPIOx: Port to access
+  * @param  GPIO_PIN: Pin to write 
+  * @param  GPIO_STATE: Set or Clear the pin 
+  * @retval None
+**/
 void HAL_GPIO_WritePin(uint16_t GPIOx, uint16_t GPIO_PIN, uint16_t GPIO_STATE);
 
 #ifdef __cplusplus
